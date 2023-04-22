36c095b4ba1e70c90572023025e40873a937b7ff
aozp73 <aozp73@naver.com>
2023-04-23 08:28:42 +0900

    initial commit

---
+++ src/test/java/shop/mtcoding/s3_sample/S3SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.s3_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
